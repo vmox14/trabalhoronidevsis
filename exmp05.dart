bc1287c2e4fa333e6b3d804e7ffe22e4d1b54006
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:20:55 -0300

    Create exmp05.dart

---
+++ exmp05.dart
@@ -0,0 +1,7 @@
+void main (){
+  double gas=5.50;
+  double pago=100;
+  double litro=pago/gas;
+  int result=litro.truncate();
+  print("Com $pago reais de gasolina, você conseguiu colocar $result litros! Parabéns");
+}
