b718191629a5d6714180fe16140e91aaff242922
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:25:34 -0300

    Create exmp10.dart

---
+++ exmp10.dart
@@ -0,0 +1,7 @@
+void main (){
+  int sandu=7;
+  double queijo=sandu*0.1;
+  double pres=sandu*0.05;
+  double rodela=sandu*0.100;
+  print("Para fazer $sandu sanduíches(s) será necessário comprar $queijo kg de queijo, $pres kg de presunto e $rodela kg de hambúrgues");
+}
