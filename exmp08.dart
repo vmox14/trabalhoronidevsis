e653df17a8274116b132793e2bddbd02f05d282b
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:23:44 -0300

    Create exmp08.dart

---
+++ exmp08.dart
@@ -0,0 +1,7 @@
+void main (){
+  int camp=2;
+  int camm=1;
+  int camg=3;
+  int valor=((camp*10)+(camm*12)+(camg*15));
+  print("Sua compra possui $camp camisa(s) pequena(s), $camm camisa(s) média(s) e $camg camisa(s) grande(s). O total da sua compra é de $valor reais");
+}
