ff5eba648943f8e1b42a9948bc4fffbdece45d96
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:15:54 -0300

    Create exmp03.dart

---
+++ exmp01.dart
@@ -0,0 +1,8 @@
+void main (){
+  int pao=100;
+  int broa=50;
+  double total=((pao*0.12)+(broa*1.50));
+  double porc=(total*10)/100;
+  print("O total vendido no dia em reais foi de $total reais");
+  print("E o valor a ser guardado é de: $porc reais");
+}
