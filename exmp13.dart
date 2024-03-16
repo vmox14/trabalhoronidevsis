3a8171288a5a82117a68ae439ed70532a663177c
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:29:53 -0300

    Create exmp13.dart

---
+++ exmp13.dart
@@ -0,0 +1,6 @@
+void main (){
+  double suco=10;
+  double maracuja=(suco-((suco*80)/100));
+  double agua=(suco-maracuja);
+  print("Será(ão) necessário(s) $agua litro(s) de água e $maracuja litro(s) de maracujá para fazer $suco litro(s) de suco");
+}
