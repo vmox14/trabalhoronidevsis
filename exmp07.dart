15c9e2d142030fd95b2547c8a6300081c1ba7d5f
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:21:55 -0300

    Create exmp07.dart

---
+++ exmp07.dart
@@ -0,0 +1,7 @@
+void main (){
+  double av1=6.5;
+  double av2=7;
+  double av3=9.7;
+  int mediap=((av1+(av2*2)+(av3*3))/1+2+3).truncate();
+  print("A média ponderada das três avaliações é $mediap");
+}
