806645308a1a0776b0830c8c7a6c24e6ffd5ce1d
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:30:38 -0300

    Create exmp15.dart

---
+++ exmp15.dart
@@ -0,0 +1,7 @@
+void main (){
+  double salario=1650;
+  double vendas=7000;
+  double comissao=(vendas*4)/100;
+  double salfinal=salario+comissao;
+  print("Foram vendidos $vendas reais neste mês, portanto o salário somado à comissão, resulta no valor de $salfinal reais");
+}
