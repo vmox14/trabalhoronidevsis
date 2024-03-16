c255f1122c8a3cc3f956dee10936c6e9e600b713
vmox14 <144456643+vmox14@users.noreply.github.com>
2024-03-16 11:28:11 -0300

    Create exmp12.dart

---
+++ exmp12.dart
@@ -0,0 +1,6 @@
+void main (){
+  int granja=67;
+  double chip=4.00;
+  double anelA=3.50;
+  double total=(granja*((2*anelA)+chip));
+  print("Serão necessários $total reais para marcar todos os frangos da granja");
